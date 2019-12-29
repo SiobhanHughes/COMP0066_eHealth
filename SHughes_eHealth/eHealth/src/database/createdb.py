3b1a3c89dd90ee98250177fbee74449f4a8bf80a
SiobhanHughes <siobhanmahughes@gmail.com>
2019-12-29 20:06:40 +0000

    create eHealth database and default admin

--- SHughes_eHealth/eHealth/src/database/createdb.py
+++ SHughes_eHealth/eHealth/src/database/createdb.py
@@ -1,5 +1,4 @@
 #https://www.sqlitetutorial.net/sqlite-python/create-tables/
-#FOREIGN KEY (project_id) REFERENCES projects (id)
 
 import connect
 import sqlite3
@@ -18,16 +17,30 @@ def create_table(conn, create_table_sql):
     except Error as e:
         print(e)
  
+def create_admin(conn, admin):
+    """ create admin in the Admin table
+    :param conn: Connection object
+    :param admin: details to be inserted into Admin table """
+    
+    sql = """ INSERT INTO Admin (administrator, passwd) VALUES (? , ?); """
+    
+    try:
+        c = conn.cursor()
+        c.execute(sql, admin)
+    except Error as e:
+        print(e)
  
 def main():
     database = r"eHealth.db"
  
-    create_admin_table = """ CREATE TABLE IF NOT EXISTS admin (
-                                        administrator text DEFAULT "admin" NOT NULL,
-                                        passwd text DEFAULT "admin" NOT NULL
+    create_admin_table = """ CREATE TABLE IF NOT EXISTS Admin (
+                                    administrator text NOT NULL,
+                                    passwd text NOT NULL
                                     ); """
+                                    
+    admin = ('admin', 'admin')
  
-    create_gp_table = """CREATE TABLE IF NOT EXISTS gp (
+    create_gp_table = """CREATE TABLE IF NOT EXISTS GPs (
                                     id integer PRIMARY KEY AUTOINCREMENT,
                                     fname text NOT NULL,
                                     lname text NOT NULL,
@@ -40,17 +53,121 @@ def main():
                                     active text DEFAULT "yes" NOT NULL
                                 );"""
  
+    create_patient_table = """CREATE TABLE IF NOT EXISTS Patients (
+                                    id integer PRIMARY KEY AUTOINCREMENT,
+                                    fname text NOT NULL,
+                                    lname text NOT NULL,
+                                    email text NOT NULL,
+                                    passwd text,
+                                    street text NOT NULL,
+                                    city text NOT NULL,
+                                    postcode text NOT NULL,
+                                    tel text NOT NULL,
+                                    contact_fname text NOT NULL,
+                                    contact_lname text NOT NULL,
+                                    contact_email text NOT NULL,
+                                    contact_street text NOT NULL,
+                                    contact_city text NOT NULL,
+                                    contact_postcode text NOT NULL,
+                                    contact_tel text NOT NULL,
+                                    active text DEFAULT "yes" NOT NULL
+                                );"""
+    
+    create_patient_record_table = """CREATE TABLE IF NOT EXISTS Patient_Record (
+                                    NHSno text NOT NULL PRIMARY KEY,
+                                    patientid integer NOT NULL,
+                                    DOB text NOT NULL,
+                                    allergies text NOT NULL,
+                                    medical_conditions text NOT NULL,
+                                    disabilities text NOT NULL,
+                                    smoker text NOT NULL,
+                                    alcohol_units_per_week integer NOT NULL,
+                                    exercise text NOT NULL,
+                                    FOREIGN KEY (patientid) REFERENCES Patients (id)
+                                );"""
+                                
+    create_vaccine_record_table = """CREATE TABLE IF NOT EXISTS Vaccine_Record (
+                                    NHSno text NOT NULL,
+                                    patientid integer NOT NULL,
+                                    date text NOT NULL,
+                                    vaccine text NOT NULL,
+                                    FOREIGN KEY (patientid) REFERENCES Patients (id),
+                                    FOREIGN KEY (NHSno) REFERENCES Patient_Record (NHSno),
+                                    PRIMARY KEY (NHSno, date, vaccine)
+                                );"""
+    
+    create_medical_history_table = """CREATE TABLE IF NOT EXISTS Medical_History (
+                                    NHSno text NOT NULL,
+                                    patientid integer NOT NULL,
+                                    gpid integer NOT NULL,
+                                    date text NOT NULL,
+                                    record text NOT NULL,
+                                    FOREIGN KEY (patientid) REFERENCES Patients (id),
+                                    FOREIGN KEY (gpid) REFERENCES GPs (id),
+                                    FOREIGN KEY (NHSno) REFERENCES Patient_Record (NHSno),
+                                    PRIMARY KEY (NHSno, gpid, date)
+                                );"""
+    
+    create_prescriptions_table = """CREATE TABLE IF NOT EXISTS Presciptions (
+                                    NHSno text NOT NULL,
+                                    patientid integer NOT NULL,
+                                    gpid integer NOT NULL,
+                                    medication text NOT NULL,
+                                    dosage text NOT NULL,
+                                    startDate text NOT NULL,
+                                    endDate text NOT NULL,
+                                    repeatScript text DEFAULT "no" NOT NULL,
+                                    repeatEnd text,
+                                    FOREIGN KEY (patientid) REFERENCES Patients (id),
+                                    FOREIGN KEY (gpid) REFERENCES GPs (id),
+                                    FOREIGN KEY (NHSno) REFERENCES Patient_Record (NHSno),
+                                    PRIMARY KEY (NHSno, gpid, medication)
+                                );"""
+                                
+    create_appointments_table = """CREATE TABLE IF NOT EXISTS Appointments (
+                                    patientid integer,
+                                    gpid integer NOT NULL,
+                                    date text NOT NULL,
+                                    time text NOT NULL,
+                                    available text DEFAULT "yes" NOT NULL,
+                                    FOREIGN KEY (patientid) REFERENCES Patients (id),
+                                    FOREIGN KEY (gpid) REFERENCES GPs (id),
+                                    PRIMARY KEY (gpid, date, time)
+                                );"""
+    
+    
     # create a database connection
     conn = connect.create_connection(database)
     print(conn)
  
     # create tables
     if conn is not None:
-        # create projects table
+        # create admin table
         create_table(conn, create_admin_table)
+        
+        #insert admin details
+        create_admin(conn, admin)
  
-        # create tasks table
+        # create gp table
         create_table(conn, create_gp_table)
+        
+        # create patient table
+        create_table(conn, create_patient_table)
+        
+        # create patient record table
+        create_table(conn, create_patient_record_table)
+        
+        # create vaccine record table
+        create_table(conn, create_vaccine_record_table)
+        
+        # create medical history table
+        create_table(conn, create_medical_history_table)
+        
+        # create precriptions table
+        create_table(conn, create_prescriptions_table)
+        
+        # create apointments table
+        create_table(conn, create_appointments_table)
         
         #save changes
         conn.commit()
--- SHughes_eHealth/readme.md
+++ SHughes_eHealth/readme.md
@@ -3,3 +3,6 @@ Siobhan Hughes
 student number 18122424
 COMP0066
 
+1. Create sql database: createdb.py will create the eHealth database (all tables and default admin) in the directory you are currently in. Natvigate to /SHughes_eHealth/eHealth/src/database and run createdb.py to create the eHealth databse in this directory.
+2. Admin table in eHealth database: this table conatins 2 columns (administrator, passwd) which has the default settings 'admin', 'admin' already saved in the database. This table is used to store the administrator username (admin) and password (admin). When admin first logs in using the password 'admin', the system will ask the admin to change the password to a more secure one. The admin will log in using the username 'admin' and the chosen password from then on.
+3. Very first log in to the eHealth application as administrator (admind): username = 'admin', password = 'admin'
