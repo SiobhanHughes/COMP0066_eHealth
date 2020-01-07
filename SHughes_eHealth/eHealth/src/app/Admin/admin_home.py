77c8486da9fc6fc49d5431c8086dcbe81377b99c
SiobhanHughes <siobhanmahughes@gmail.com>
2020-01-07 16:44:15 +0000

    Admin Home GUI started

--- SHughes_eHealth/eHealth/src/app/Admin/admin_home.py
+++ SHughes_eHealth/eHealth/src/app/Admin/admin_home.py
@@ -4,6 +4,7 @@
 #============================IMPORT============================================
 
 import tkinter as tk
+from tkinter import ttk
 
 import os
 import sys
@@ -24,6 +25,7 @@ path.insert_dir(eHealth_dir)
 from src.database import db_utilities as dbu
 from src.database import connect
 from src.utilities import track_user as track
+from src.app.GUI import outter_scroll_frame
 path.delete_dir()
 
 
@@ -42,19 +44,120 @@ class Admin(tk.Frame):
     def show(self):
         self.lift()
 
-class Admin1(Admin):
+class Homepage(Admin):
    def __init__(self, *args, **kwargs):
        Admin.__init__(self, *args, **kwargs)
-       label = tk.Label(self, text="This is Admin 1")
-       label.pack(side="top", fill="both", expand=True)
+       
+        #==============================VARIABLES======================================
+       self.patient_fname = tk.StringVar()
+       self.patient_lname = tk.StringVar()
+       self.gp_fname = tk.StringVar()
+       self.gp_lname = tk.StringVar()
 
-class Admin2(Admin):
+            #==============================FRAMES=========================================
+       self.Form = tk.Frame(self, height=200)
+       self.Form.pack(side=tk.TOP, pady=20)
+
+                #==============================LABELS=========================================
+       self.lbl_email = tk.Label(self.Form, text = "Send 24h appointment reminder email", font=('arial', 14), bd=15, fg='blue')
+       self.lbl_email.grid(row=0, sticky="e")
+       self.lbl_text = tk.Label(self.Form) #error messages appear here
+       self.lbl_text.grid(row=2, columnspan=2)
+       self.lbl_password = tk.Label(self.Form, text = "SEARCH", font=('arial', 14), bd=15, fg='green')
+       self.lbl_password.grid(row=3, sticky="e")
+       self.lbl_password = tk.Label(self.Form, text = "Patient First Name:", font=('arial', 14), bd=15)
+       self.lbl_password.grid(row=4, sticky="e")
+       self.lbl_password = tk.Label(self.Form, text = "Patient Last Name:", font=('arial', 14), bd=15)
+       self.lbl_password.grid(row=4, column= 1,sticky="e")
+       self.lbl_password = tk.Label(self.Form, text = "GP First Name:", font=('arial', 14), bd=15)
+       self.lbl_password.grid(row=6, sticky="e")
+       self.lbl_password = tk.Label(self.Form, text = "GP Last Name:", font=('arial', 14), bd=15)
+       self.lbl_password.grid(row=6, column= 1,sticky="e")
+
+
+                #==============================ENTRY WIDGETS==================================
+       self.patient_fname = tk.Entry(self.Form, textvariable=self.patient_fname, font=(14))
+       self.patient_fname.grid(row=5)
+       self.patient_lname = tk.Entry(self.Form, textvariable=self.patient_lname, font=(14))
+       self.patient_lname.grid(row=5, column=1)
+       self.gp_fname = tk.Entry(self.Form, textvariable=self.gp_fname, font=(14))
+       self.gp_fname.grid(row=7)
+       self.gp_lname = tk.Entry(self.Form, textvariable=self.gp_lname, font=(14))
+       self.gp_lname.grid(row=7, column=1)
+
+                #==============================BUTTON WIDGETS=================================
+       self.btn_email = tk.Button(self.Form, text="email", width=45, command=self.reminder_email, fg='blue')
+       self.btn_email.grid(pady=25, row=1, columnspan=2)
+       self.btn_search = tk.Button(self.Form, text="Search", width=45, command=self.search, fg='green')
+       self.btn_search.grid(pady=25, row=8, columnspan=2)
+       self.btn_clear = tk.Button(self.Form, text="Clear all fields", width=45, command=self.search, fg='green')
+       self.btn_clear.grid(pady=25, row=9, columnspan=2)
+       
+   def reminder_email(self):
+       pass
+   
+   def search(self):
+       pass
+   
+   def clear(self):
+       pass
+
+class Edit(Admin):
+    def __init__(self, *args, **kwargs):
+        Admin.__init__(self, *args, **kwargs)
+       
+         #==============================VARIABLES======================================
+        self.patient_id = tk.StringVar()
+        self.gp_id = tk.StringVar()
+             
+             #==============================FRAMES=========================================
+        self.Form = tk.Frame(self, height=200)
+        self.Form.pack(side=tk.TOP, pady=20)
+
+                 #==============================LABELS=========================================
+        self.lbl_password = tk.Label(self.Form, text = "EDIT: enter a Patient or GP id number (use the search function)", font=('arial', 14), bd=15, fg='green')
+        self.lbl_password.grid(row=0, sticky="e")
+        self.lbl_text = tk.Label(self.Form) #error messages appear here
+        self.lbl_text.grid(row=1, columnspan=2)
+        self.lbl_password = tk.Label(self.Form, text = "Patient id:", font=('arial', 14), bd=15)
+        self.lbl_password.grid(row=2, sticky="w")
+        self.lbl_password = tk.Label(self.Form, text = "GP id:", font=('arial', 14), bd=15)
+        self.lbl_password.grid(row=4, sticky="w")
+
+
+                 #==============================ENTRY WIDGETS==================================
+        self.patient_id = tk.Entry(self.Form, textvariable=self.patient_id, font=(14))
+        self.patient_id.grid(row=3)
+        self.gp_id = tk.Entry(self.Form, textvariable=self.gp_id, font=(14))
+        self.gp_id.grid(row=5)
+
+                 #==============================BUTTON WIDGETS=================================
+        self.btn_edit = tk.Button(self.Form, text="Edit", width=45, command=self.edit, fg='green')
+        self.btn_edit.grid(pady=25, row=6, columnspan=2)
+        self.btn_delete = tk.Button(self.Form, text="Delete", width=45, command=self.delete, fg='red')
+        self.btn_delete.grid(pady=25, row=7, columnspan=2)
+        self.btn_deactivate = tk.Button(self.Form, text="Deactivate", width=45, command=self.deactivate, fg='blue')
+        self.btn_deactivate.grid(pady=25, row=8, columnspan=2)
+
+    
+    def edit(self):
+        pass
+    
+    def delete(self):
+        pass
+    
+    def deactivate(self):
+        pass
+       
+
+class Register_Patient(Admin):
    def __init__(self, *args, **kwargs):
        Admin.__init__(self, *args, **kwargs)
+       scroll_win = outter_scroll_frame.ScrolledFrame(self)
        label = tk.Label(self, text="This is Admin 2")
        label.pack(side="top", fill="both", expand=True)
 
-class Admin3(Admin):
+class Add_GP(Admin):
    def __init__(self, *args, **kwargs):
        Admin.__init__(self, *args, **kwargs)
        label = tk.Label(self, text="This is Admin 3")
@@ -63,32 +166,42 @@ class Admin3(Admin):
 class MainView(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
-        lbl_title = tk.Label(self, text = "Admin: eHealth system", font=('arial', 15))
+        
+        Top = tk.Frame(bd=2,  relief=tk.RIDGE)
+        Top.pack(side=tk.TOP, fill=tk.X)
+        lbl_title = tk.Label(Top, text = "Admin: eHealth", font=('arial', 15))
         lbl_title.pack(fill=tk.X)
-        p1 = Admin1(self)
-        p2 = Admin2(self)
-        p3 = Admin3(self)
-
-        buttonframe = tk.Frame(self)
+        
+        buttonframe = tk.Frame(self, Top)
         container = tk.Frame(self)
         buttonframe.pack(side="top", fill="x", expand=False)
         container.pack(side="top", fill="both", expand=True)
+        
+        home = Homepage(self)
+        edit = Edit(self)
+        new_patient = Register_Patient(self)
+        new_gp = Add_GP(self)
+    
+        home.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        edit.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        new_patient.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        new_gp.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
 
-        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        btn_home = tk.Button(buttonframe, text="Home", command=home.lift)
+        btn_edit = tk.Button(buttonframe, text="Edit", command=edit.lift)
+        btn_patient = tk.Button(buttonframe, text="Register Patient", command=new_patient.lift)
+        btn_gp = tk.Button(buttonframe, text="Add GP", command=new_gp.lift)
+        
+        btn_logout = tk.Button(buttonframe, text='logout', command=self.logout, fg='red')
 
-        b1 = tk.Button(buttonframe, text="Admin 1", command=p1.lift)
-        b2 = tk.Button(buttonframe, text="Admin 2", command=p2.lift)
-        b3 = tk.Button(buttonframe, text="Admin 3", command=p3.lift)
-        btn_back = tk.Button(buttonframe, text='logout', command=self.logout)
+        btn_home.pack(side="left")
+        btn_edit.pack(side="left")
+        btn_patient.pack(side="left")
+        btn_gp.pack(side="left")
 
-        b1.pack(side="left")
-        b2.pack(side="left")
-        b3.pack(side="left")
-        btn_back.pack(side="right")
+        btn_logout.pack(side="right")
 
-        p1.show()
+        home.show()
         
     def connect_to_db(self):
         global conn, cursor
@@ -111,6 +224,6 @@ if __name__ == "__main__":
     main = MainView(root)
     main.pack(side="top", fill="both", expand=True)
     root.title("Welcome to the eHealth system")
-    root.wm_geometry("400x400")
+    root.wm_geometry("900x900")
     main.bind('<Destroy>', close)
     root.mainloop()
---
+++ SHughes_eHealth/eHealth/src/app/GUI/outter_scroll_frame.py
@@ -0,0 +1,92 @@
+#ScrolledFrame template taken from:
+#https://stackoverflow.com/questions/47727404/how-to-add-a-scrollbar-to-window-made-with-tkinter
+
+import tkinter as tk
+from random import randint
+
+# --- classes ---
+
+class ScrolledFrame(tk.Frame):
+
+    def __init__(self, parent, vertical=True, horizontal=False):
+        super().__init__(parent)
+
+        # canvas for inner frame
+        self._canvas = tk.Canvas(self)
+        self._canvas.grid(row=0, column=0, sticky='news') # changed
+
+        # create right scrollbar and connect to canvas Y
+        self._vertical_bar = tk.Scrollbar(self, orient='vertical', command=self._canvas.yview)
+        if vertical:
+            self._vertical_bar.grid(row=0, column=1, sticky='ns')
+        self._canvas.configure(yscrollcommand=self._vertical_bar.set)
+
+        # create bottom scrollbar and connect to canvas X
+        self._horizontal_bar = tk.Scrollbar(self, orient='horizontal', command=self._canvas.xview)
+        if horizontal:
+            self._horizontal_bar.grid(row=1, column=0, sticky='we')
+        self._canvas.configure(xscrollcommand=self._horizontal_bar.set)
+
+        # inner frame for widgets
+        self.inner = tk.Frame(self._canvas, bg='red')
+        self._window = self._canvas.create_window((0, 0), window=self.inner, anchor='nw')
+
+        # autoresize inner frame
+        self.columnconfigure(0, weight=1) # changed
+        self.rowconfigure(0, weight=1) # changed
+
+        # resize when configure changed
+        self.inner.bind('<Configure>', self.resize)
+        self._canvas.bind('<Configure>', self.frame_width)
+
+    def frame_width(self, event):
+        # resize inner frame to canvas size
+        canvas_width = event.width
+        self._canvas.itemconfig(self._window, width = canvas_width)
+
+    def resize(self, event=None): 
+        self._canvas.configure(scrollregion=self._canvas.bbox('all'))
+
+class Question:
+
+    def __init__(self, parent, question, answer):
+        self.parent = parent
+        self.question = question
+        self.answer = answer
+        self.create_widgets()
+
+    def get_input(self):
+        value = self.entry.get()
+        print('value:', value)
+        if value == self.answer:
+            print("Esatto. è " + self.answer)
+            self.label['text'] = "Esatto"
+
+    def create_widgets(self):
+        self.labelframe = tk.LabelFrame(self.parent, text="Domanda:")
+        self.labelframe.pack(fill="both", expand=True)
+
+        self.label = tk.Label(self.labelframe, text=self.question)
+        self.label.pack(expand=True, fill='both')
+
+        self.entry = tk.Entry(self.labelframe)
+        self.entry.pack()
+
+        self.button = tk.Button(self.labelframe, text="Click", command=self.get_input)
+        self.button.pack()
+
+# --- main ---
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title("Quiz")
+    root.geometry("400x300")
+
+    window = ScrolledFrame(root)
+    window.pack(expand=True, fill='both')
+
+    for i in range(10):
+        one = randint(1, 10)
+        two = randint(1, 10)
+        Question(window.inner, "Quanto fa {} + {} ?".format(one, two), str(one + two))
+
+    root.mainloop()
